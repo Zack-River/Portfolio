705a81f68b643fa6636bcc51b15ddffd82674813
Zack-River <elmutamiz@gmail.com>
2026-06-30 18:20:40 +0300

    fix routing + images problem

--- fix_lines.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let lines = fs.readFileSync('components/Scene3D.tsx', 'utf8').split('\n');
-// Delete lines 268 to 1342 (indices 267 to 1341)
-lines.splice(267, 1342 - 268 + 1);
-fs.writeFileSync('components/Scene3D.tsx', lines.join('\n'));
-console.log('Deleted lines');
